8eac6545413f602be059d03e0d79373877ae0d7e
Scott Campana <sbcampana@gmail.com>
2017-02-06 17:44:49 -0700

    added cp1.py

---
+++ cp1.py
@@ -0,0 +1,10 @@
+# Practice Problems from coding bat
+# They should each be their own function inside of this file
+# I will show you how to make a function similar to what you will need to be doing
+
+
+def main():
+	print("Hello")
+
+
+main()
