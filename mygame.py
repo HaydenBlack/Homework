d6a80090a3b44a4975016db9df566a2b84aa69fc
Hayden Black <hbla54303@adams12.org>
2017-03-01 20:44:01 -0700

    When controlled box goes over apple the apple dissapears and randomly generates a new one

--- mygame.py
+++ mygame.py
@@ -1,4 +1,10 @@
 import pygame
+import random
+color4 = (0, 0, 0)
+#Naming the Variables
+xcoordinant = random.randrange(0, 700)
+ycoordinant = random.randrange(0, 700)
+rangeY = (0, 700)
 hotpink = (255,20,147)
 color3 = (0,0,0)
 pygame.init()
@@ -11,9 +17,10 @@ y = 30
 a = 30
 b = 30
 t = 350
-g =350
+g = 350
 
 clock = pygame.time.Clock()
+#Changing the color
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -22,18 +29,18 @@ while not done:
             is_blue = not is_blue
         if event.type == pygame.KEYDOWN and event.key == pygame.K_LSHIFT:
             is_green = not is_green
-
+#Controls
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP]: y -=3
-    if pressed[pygame.K_DOWN]: y += 3
-    if pressed[pygame.K_LEFT]: x -= 3
-    if pressed[pygame.K_RIGHT]: x += 3
+    if pressed[pygame.K_UP]: y -= 1
+    if pressed[pygame.K_DOWN]: y += 1
+    if pressed[pygame.K_LEFT]: x -= 1
+    if pressed[pygame.K_RIGHT]: x += 1
 
-    if pressed[pygame.K_w]: b -= 3
-    if pressed[pygame.K_s]: b += 3
-    if pressed[pygame.K_a]: a -= 3
-    if pressed[pygame.K_d]: a += 3
-
+    if pressed[pygame.K_w]: b -= 1
+    if pressed[pygame.K_s]: b += 1
+    if pressed[pygame.K_a]: a -= 1
+    if pressed[pygame.K_d]: a += 1
+#Colors
     screen.fill(hotpink)
     if is_green: color2 = (255, 255, 0)
     else: color2 = (0, 255, 0)
@@ -41,15 +48,29 @@ while not done:
     else: color = (255, 100, 0)
 
     if ((t == x and g == y)):
+        for i in range(1):
+            pygame.draw.rect(screen,(0, 0, 0), pygame.Rect(xcoordinant, ycoordinant, 60, 60))
         color3 = hotpink
+        #random.randint(rangeX and rangeY)
     elif ((t == a and g == b)):
+        for j in range(1):
+            pygame.draw.rect(screen,(0, 0, 0), pygame.Rect(xcoordinant, ycoordinant, 60, 60))
         color3 = hotpink
-
+        #random.randint(rangeX and rangeY)
+#The rectangles traits
     screen.fill(hotpink)
     pygame.draw.rect(screen, color3, pygame.Rect(t, g, 60, 60))
     pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
     pygame.draw.rect(screen, color2, pygame.Rect(a, b, 60, 60))
+    if ((t == x and g == y)):
+        for k in range(1):
+            pygame.draw.rect(screen, color4, pygame.Rect(xcoordinant, ycoordinant, 60, 60))
+            color4 = (0, 0, 0)
 
+    elif ((t == a and g == b)):
+        for i in range(1):
+            pygame.draw.rect(screen, color4, pygame.Rect(xcoordinant, ycoordinant, 60, 60))
+        color4 = (0, 0, 0)
 
 
 
