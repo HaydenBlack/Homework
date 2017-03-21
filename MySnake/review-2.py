3648c4219563d491f4db4fdfef0afc90d0f03783
Hayden Black <hbla54303@adams12.org>
2017-03-20 19:12:13 -0600

    I completed the Homework

---
+++ MySnake/review-2.py
@@ -0,0 +1,91 @@
+#var = "Hello"
+##var2 = (1,2,3,4,5)
+#var3 = 12345
+#print(var2[2])
+#for h in range(10):
+   # print(var3[3])
+
+#Make a function for Multiplication, subtraction and division
+edd = 66
+jedd = 55
+jk = edd * jedd
+hay = 77
+sco = 8
+haysco = hay / sco
+scott = 3333333333
+hayden = 4444
+typo = scott - hayden
+def addsum(Gone, Bye):
+    Gone += Bye
+    return Gone
+
+def multisum(edd,jedd):
+    (edd * jedd == jk)
+    return jk
+
+def dividesum(hay, sco):
+    (hay / sco == haysco)
+    return haysco
+
+def subtractsum(scott, hayden):
+    (scott - hayden == typo)
+    return typo
+
+def add2(Home):
+    Home += 2
+    return Home
+
+list = []
+list2 = []
+list3 = []
+list4 = []
+#Addition
+print("Addition")
+print("")
+for g in range(10):
+   if (g % 2 == 0):
+       list.append(g)
+       print(add2(g))
+   print("")
+
+
+#Multiplication
+print("Multiplication")
+print("")
+for i in range(1):
+    if (2 == 2):
+        list2.append(jk)
+        print(multisum(edd, jedd))
+    print("")
+
+
+#Division
+print("Division")
+print("")
+for t in range(1):
+    if (3==3):
+        list3.append(haysco)
+        print(dividesum(hay, sco))
+        print("")
+
+
+#Subtraction
+print("Subtraction")
+print("")
+for k in range(1):
+    if (4==4):
+        list4.append(typo)
+        print(subtractsum(scott, hayden))
+        print("")
+
+
+"""
+print(subtractsum(scott, hayden))
+print(dividesum(hay, sco))
+print(multisum(edd, jedd))
+print(addsum(5,4))
+"""
+
+print(list)
+
+
